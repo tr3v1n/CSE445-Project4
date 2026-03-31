223742ba3d67e416c6c42720e68dedbc2b368f64
Trevin Prester <prestertrevin@gmail.com>
2026-03-31 16:45:16 -0700

    Task 2 and 3
    Submission.cs populated with correct methods and main function

--- Submission.cs
+++ Submission.cs
@@ -19,9 +19,21 @@ namespace ConsoleApp1
 
     public class Submission
     {
-        public static string xmlURL = "Your XML URL";
-        public static string xmlErrorURL = "Your Error XML URL";
-        public static string xsdURL = "Your XSD URL";
+        public static string xmlURL = "https://tr3v1n.github.io/CSE445-Project4/NationalParks.xml";
+        public static string xmlErrorURL = "https://tr3v1n.github.io/CSE445-Project4/NationalParksErrors.xml";
+        public static string xsdURL = "https://tr3v1n.github.io/CSE445-Project4/NationalParks.xsd";
+
+        public static void Main(string[] args)
+        {
+            string result = Verification(args[0], args[2]);
+            Console.WriteLine(result);
+
+            result = Verification(args[1], args[2]);
+            Console.WriteLine(result);
+
+            result = Xml2Json(args[0]);
+            Console.WriteLine(result);
+        }
 
         public static void Main(string[] args)
         {
@@ -41,11 +53,46 @@ namespace ConsoleApp1
         public static string Verification(string xmlUrl, string xsdUrl)
         {
             //return "No Error" if XML is valid. Otherwise, return the desired exception message.
+            string errorMessage = null;
+
+            XmlSchemaSet sc = new XmlSchemaSet();
+
+            sc.Add(null, xsdUrl);
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas = sc;
+
+            settings.ValidationEventHandler += (sender, e) =>
+            {
+                errorMessage = e.Message;
+            };
+
+            try
+            {
+                using (XmlReader reader = XmlReader.Create(xmlUrl, settings))
+                {
+                    while (reader.Read()) { }
+                }
+            } catch (Exception e)
+            {
+                return e.Message;
+            }
+            if (errorMessage == null)
+            {
+                errorMessage = "No Error";
+            }
+            return errorMessage;
         }
 
         public static string Xml2Json(string xmlUrl)
         {
             // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlUrl);
+
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+            return jsonText;
         }
 
         // Helper method to download content from URL
